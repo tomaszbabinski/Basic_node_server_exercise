e0c3bb7faf58847b7a3b96810359044c837a8d63
tomaszbabinski <tommybab@gmail.com>
2020-07-17 16:53:31 +0200

    First routing requests

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const http = require('http');
 
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
+
+    const url = req.url;
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
     res.setHeader('Content-type','text/html');
     res.write('<html>');
     res.write('<head><title>My first page</title></head>')
