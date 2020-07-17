91bda47baadc88808f539191b9eedc3dc2678734
tomaszbabinski <tommybab@gmail.com>
2020-07-17 16:38:55 +0200

    Sending sample response from the server

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
     console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>')
+    res.write('<body><h1>Hello from my Node.js</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
