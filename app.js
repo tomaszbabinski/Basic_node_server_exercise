f64ac8ba4d3098267b6d4f396c0c2a2f314a29e1
tomaszbabinski <tommybab@gmail.com>
2020-07-17 16:27:18 +0200

    Checking request url/method/headers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
 });
 
 server.listen(3000);
