1c98eae5e27828cc3e3b775608342f8aad16c51a
tomaszbabinski <tommybab@gmail.com>
2020-07-21 21:32:25 +0200

    Redirecting requests

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const http = require('http');
-
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
 
     const url = req.url;
+    const method = req.method;
     if(url==='/'){
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if(url==='/message' && method ==='POST'){
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location','/');
         return res.end();
     }
 
@@ -21,4 +28,3 @@ const server = http.createServer((req,res)=>{
 });
 
 server.listen(3000);
-
