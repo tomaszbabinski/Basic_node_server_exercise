6545f3319abcc4de8694ddd777d12a4d27729487
tomaszbabinski <tommybab@gmail.com>
2020-07-21 21:59:25 +0200

    Parsing requests bodies

--- app.js
+++ app.js
@@ -13,8 +13,19 @@ const server = http.createServer((req,res)=>{
         return res.end();
     }
     if(url==='/message' && method ==='POST'){
-        fs.writeFileSync('message.txt','DUMMY');
-        res.statusCode = 302;
+        const body = [];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        })
+
+        req.on('end',()=> {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+        })
+        res.statusCode = 302; 
         res.setHeader('Location','/');
         return res.end();
     }
