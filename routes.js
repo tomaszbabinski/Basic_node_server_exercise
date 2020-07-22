06f4604036e9e315378b9bef359d35b0cc95a5f6
tomaszbabinski <tommybab@gmail.com>
2020-07-22 22:03:51 +0200

    Another way of importing from other files

--- routes.js
+++ routes.js
@@ -36,5 +36,7 @@ const requestHandler = (req,res) => {
     res.end();
 }
 
-module.exports = requestHandler;
-
+// module.exports = requestHandler;
+module.exports = {
+    handler: requestHandler
+}
