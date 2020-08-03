1f32fdf8ddafaa39af7698847d0fbe98bcd1ac97
tomaszbabinski <tommybab@gmail.com>
2020-08-03 20:54:18 +0200

    removing routes to add express.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes.handler);
-    
+
+const server = http.createServer();
+
 server.listen(3000);
