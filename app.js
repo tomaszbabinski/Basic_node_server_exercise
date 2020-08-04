57013aa7e042148383629a210a82bcd23667b777
tomaszbabinski <tommybab@gmail.com>
2020-08-04 21:26:32 +0200

    Parsing incoming requests exercise

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+    
 });
 
-app.use((req,res,next)=>{
-    console.log('In another middleware');
+app.use('/product',(req,res,next)=> {
+    console.log(req.body);
+    res.redirect("/");
+})
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello from the express.js</form>')
     
 });
 
