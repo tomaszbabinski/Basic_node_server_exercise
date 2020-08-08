14f8831f7da765b8a09b8a02954807914c3cebb4
tomaszbabinski <tommybab@gmail.com>
2020-08-08 12:22:31 +0200

    Another way of getting proper path for our html's

--- router/admin.js
+++ router/admin.js
@@ -2,10 +2,11 @@ const express = require('express')
 const path = require('path');
 
 const router = express.Router()
+const rootDir = require('../util/path');
 
 //this normally gets /admin/add-product -> GET
 router.get('/add-product',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
+    res.sendFile(path.join(rootDir,'views','add-product.html'))
     
 });
 
--- router/shop.js
+++ router/shop.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
+const rootDir = require('../util/path');
 
 router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../','views','shop.html'));
+    res.sendFile(path.join(rootDir,'views','shop.html'));
     
 });
 
