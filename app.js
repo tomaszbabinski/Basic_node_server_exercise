7fdd7512d34626715c70ea2e2abfe43a3218a5a1
tomaszbabinski <tommybab@gmail.com>
2020-08-08 12:11:23 +0200

    404 page added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=> {
-    res.status(404).send("<h1>Page not found</h1>")
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
 app.listen(3000);
