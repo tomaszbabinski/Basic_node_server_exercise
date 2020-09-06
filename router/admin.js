7ebcf2acb91ec6cdd679cec16e340f269525b746
tomaszbabinski <tommybab@gmail.com>
2020-09-06 15:45:49 +0200

    Adding active class once the add-product is reached

--- router/admin.js
+++ router/admin.js
@@ -9,7 +9,7 @@ const products = [];
 //this normally gets /admin/add-product -> GET
 router.get('/add-product', (req, res, next) => {
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-    res.render('add-product',{pageTitle: 'Add product'});
+    res.render('add-product',{pageTitle: 'Add product', path: '/admin/add-product'});
 
 });
 
