76382edd1004076cc67d7047c18fa05e5a740f2e
tomaszbabinski <tommybab@gmail.com>
2020-09-06 16:04:00 +0200

    Finishing other pug templates

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(shopRoutes);
 
 app.use((req,res,next)=> {
     // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: 'Page not found!!!'});
 })
 
 app.listen(3000);
