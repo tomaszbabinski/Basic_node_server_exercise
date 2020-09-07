3c6af7bd49c4f4b5abd521ee5b03cfe253849077
tomaszbabinski <tommybab@gmail.com>
2020-09-07 21:13:53 +0200

    Working on common layoyt for hbs files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs());
+app.engine('hbs', expressHbs({layoutsDir: 'views/layoyts/',defaultLayout: 'main-layout'}));
 app.set('view engine','hbs');
 app.set('views','views');
 
