89d82dc376b59a2a9e8b726fd09923c71a744c8d
tomaszbabinski <tommybab@gmail.com>
2020-09-07 20:17:53 +0200

    Adding handlebars to the project and swtiching 404 from pug to hbs

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine','pug');
+app.engine('hbs', expressHbs());
+app.set('view engine','hbs');
 app.set('views','views');
 
 const adminData = require('./router/admin');
