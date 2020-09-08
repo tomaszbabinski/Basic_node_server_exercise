a6ac566cd359e59865a84ec6cb36fc38f768717d
tomaszbabinski <tommybab@gmail.com>
2020-09-08 21:07:33 +0200

    Changing view engine to EJS

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/',defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine','hbs');
+
+app.set('view engine','ejs');
 app.set('views','views');
 
 const adminData = require('./router/admin');
