ea44dc09b6182b8ecacf218d608d5af158d4633d
tomaszbabinski <tommybab@gmail.com>
2020-09-14 20:44:57 +0200

    Fixing an error for 404 - path attribute added

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(shopRoutes);
 
 app.use((req,res,next)=> {
     // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-    res.status(404).render('404', {pageTitle: 'Page not found!!!'});
+    res.status(404).render('404', {pageTitle: 'Page not found!!!',path: ''});
 })
 
 app.listen(3000);
