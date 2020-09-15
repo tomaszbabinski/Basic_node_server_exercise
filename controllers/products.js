58760b85ad074d30a572eac2e4571ba34ea8effa
tomaszbabinski <tommybab@gmail.com>
2020-09-15 21:58:50 +0200

    MVC implementing for shop.js '/'

--- controllers/products.js
+++ controllers/products.js
@@ -15,3 +15,14 @@ exports.postAddProduct = (req, res, next) => {
     products.push({title: req.body.title});
     res.redirect("/");
 }
+
+exports.getProducts = (req, res, next) => {
+    res.render('shop',{
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true });
+
+}
--- router/shop.js
+++ router/shop.js
@@ -2,23 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
-const rootDir = require('../util/path');
 
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
-router.get('/', (req, res, next) => {
-    // console.log(adminData.products);
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    const products = adminData.products;
-
-    res.render('shop',{
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true });
-
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
