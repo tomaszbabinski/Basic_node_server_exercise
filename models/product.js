298b767dce0ed0892bb269fb830df765b8dc3609
tomaszbabinski <tommybab@gmail.com>
2020-09-25 19:23:03 +0200

    Extracting dynamic param from the path

--- models/product.js
+++ models/product.js
@@ -22,7 +22,7 @@ module.exports = class Product{
         this.price = price;
     }
     save(){
-        this.is = Math.random().toString();
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p,JSON.stringify(products),(err) => {
--- router/shop.js
+++ router/shop.js
@@ -6,7 +6,10 @@ const router = express.Router();
 const shopController = require('../controllers/shop');
 
 router.get('/', shopController.getIndex);
+
 router.get('/products',shopController.getProducts);
+
+router.get('/products/:productId',shopController.getProduct);
 
 router.get('/cart',shopController.getCart);
 
