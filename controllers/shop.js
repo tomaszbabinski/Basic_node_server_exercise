ed735b555eca028d1efbaf2457497bd12b715a7e
tomaszbabinski <tommybab@gmail.com>
2020-09-28 21:16:58 +0200

    Updating postCart method in shop controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
@@ -43,8 +43,11 @@ exports.getCart = (req,res,next) => {
     });
 }
 
-exports.postCar = (req,res,next) => {
+exports.postCart = (req,res,next) => {
     const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+        Cart.addProduct(prodId, product.price);
+    })
     res.redirect('/cart');
 };
 
