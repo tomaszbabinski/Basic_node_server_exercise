060ae20a9b8061cceb2b15dc971190f87434f590
tomaszbabinski <tommybab@gmail.com>
2020-09-30 20:27:15 +0200

    Editing product data added to controller and route

--- controllers/admin.js
+++ controllers/admin.js
@@ -31,7 +31,15 @@ exports.getEditProduct = (req, res, next) => {
 }
 
 exports.postEditProduct = (req,res,next) => {
-    
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+
+    const updatedProduct = new Product(prodId,updatedTitle,updatedImageUrl,updatedDescription,updatedPrice);
+    updatedProduct.save();
+    res.redirect('/products');
 }
 
 exports.postAddProduct = (req, res, next) => {
@@ -40,7 +48,7 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
 
-     const product = new Product(title,imageUrl,description,price);
+     const product = new Product(null,title,imageUrl,description,price);
      product.save();
     res.redirect('/');
 }
