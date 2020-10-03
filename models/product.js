621221f450bb00de4599308d5de00aa67cd21d2c
tomaszbabinski <tommybab@gmail.com>
2020-10-03 14:08:39 +0200

    Adding deleteById method in Product model

--- models/product.js
+++ models/product.js
@@ -53,4 +53,15 @@ module.exports = class Product{
             cb(product);
         })
     }
+
+    static deleteById(id){
+        getProductsFromFile(products => {
+            const updatedProducts = products.filter(prod => prod.id !== id);
+            fs.writeFile(p,JSON.stringify(updatedProducts),err => {
+                if(!err){
+
+                }
+            });
+        });
+    }
 };
