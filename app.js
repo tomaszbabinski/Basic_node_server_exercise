9b56f383e79e3ae752cf5e6e0f272d200f75f7bb
tomaszbabinski <tommybab@gmail.com>
2020-10-12 22:10:54 +0200

    Running sample code to check if we can connect and retrieve from db

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const app = express();
 
+const db = require('./util/database');
 
 app.set('view engine','ejs');
 app.set('views','views');
@@ -11,6 +12,14 @@ app.set('views','views');
 const adminRoutes = require('./router/admin');
 const shopRoutes = require('./router/shop');
 const errorController = require('./controllers/error');
+
+db.execute('SELECT * FROM products')
+    .then((result)=> {
+        console.log(result);
+    })
+    .catch(err => {
+        console.log(err)
+    });
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
