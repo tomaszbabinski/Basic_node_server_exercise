999c91f12dd82066d542d6d301c98004924552c9
tomaszbabinski <tommybab@gmail.com>
2020-10-13 20:44:21 +0200

    Changing getProducts in Shop controller for SQL requirements

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,15 +2,15 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
-        res.render('shop/product-list',{
-            prods: products,
-            pageTitle: 'All Products',
-            path: '/products',
-         });
-    
-    }); 
-    
+    Product.fetchAll()
+        .then(([rows,fieldData]) => {
+            res.render('shop/product-list',{
+                prods: rows,
+                pageTitle: 'All Products',
+                path: '/products',
+             });
+        })
+        .catch(err => { console.log(err)});   
 }
 
 exports.getProduct = (req,res,next) => {
