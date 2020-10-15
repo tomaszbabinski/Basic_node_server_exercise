422a3e82dcc7d4ab0979781b5297dcd2d31fe4c8
tomaszbabinski <tommybab@gmail.com>
2020-10-15 20:43:10 +0200

    Adding connection to the db using sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'coderslab'
+const sequelize = new Sequelize('node-complete','root','coderslab',{
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
