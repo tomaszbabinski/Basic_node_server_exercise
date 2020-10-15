9d66af3efcb9dc7ce8db2425a12af1eb4c232479
tomaszbabinski <tommybab@gmail.com>
2020-10-15 21:10:54 +0200

    Syncing Js model to the database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 app.set('view engine','ejs');
 app.set('views','views');
@@ -20,4 +20,9 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync().then(result => {
+    // console.log(result)
+    app.listen(3000);
+}).catch(err => {
+    console.log(err)
+});
