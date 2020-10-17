e8338bf673738cd7221309df78088f3a79a16cff
tomaszbabinski <tommybab@gmail.com>
2020-10-17 19:53:28 +0200

    Getting product by Id using sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,16 +13,23 @@ exports.getProducts = (req, res, next) => {
 }
 
 exports.getProduct = (req,res,next) => {
-    const prodId = req.params.productId;
-    Product.findById(prodId)
-        .then(([product]) => {
-            res.render('shop/product-detail', {
-                product: product[0],
-                pageTitle: product.title,
-                path: '/products'
-            });
-        })
-        .catch(err => { console.log(err)})
+    const prodId = req.params.productId; 
+    Product.findAll({where: {id: prodId}}).then(products => {
+        res.render('shop/product-detail', {
+            product: products[0],
+            pageTitle: products[0].title,
+            path: '/products'
+        });
+    }).catch(err=> console.log(err));
+    // Product.findByPk(prodId)
+    //     .then((product) => {
+    //         res.render('shop/product-detail', {
+    //             product: product,
+    //             pageTitle: product.title,
+    //             path: '/products'
+    //         });
+    //     })
+    //     .catch(err => { console.log(err)})
 }
 
 exports.getIndex = (req,res,next) => {
