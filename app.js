bca6fb8c0480c71709aec14ae713daebcce29dd4
tomaszbabinski <tommybab@gmail.com>
2020-10-21 21:39:52 +0200

    Creating a dummy user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const app = express();
 
 app.set('view engine','ejs');
 app.set('views','views');
@@ -14,10 +15,19 @@ const Product = require('./models/product')
 const adminRoutes = require('./router/admin');
 const shopRoutes = require('./router/shop');
 
-const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
+
+app.use((req,res,next) => {
+    User.findByPk(1)
+        .then( user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err))
+});
+
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
@@ -26,9 +36,22 @@ app.use(errorController.get404);
  Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
  User.hasMany(Product);
 
-sequelize.sync({force: true}).then(result => {
-    // console.log(result)
-    app.listen(3000);
-}).catch(err => {
+sequelize
+    .sync()
+    // .sync({force: true})
+    .then(result => {
+        return User.findByPk(1)
+    })
+    .then(user => {
+        if(!user){
+            return User.create({name: 'Tom', email: 'test@test.pl'});
+        }
+        return user;
+    })
+    .then(user => {
+        // console.log(user);
+        app.listen(3000);
+    })
+    .catch(err => {
     console.log(err)
 });
