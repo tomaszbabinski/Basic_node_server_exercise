099996cff2d02e7a2b67c96c210ee3835dc1239b
tomaszbabinski <tommybab@gmail.com>
2020-10-23 20:45:53 +0200

    Adjusting getProduct and GetEditProducts in admin controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,9 +16,11 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-     
-    Product.findByPk(prodId)
-        .then(product => {
+
+    req.user.getProducts({where: {id: prodId}})
+    // Product.findByPk(prodId)
+        .then(products => {
+            const product = products[0];
             if(!product){
                 return res.redirect('/');
             }
@@ -74,7 +76,7 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req,res,next) => {
-    Product.findAll()
+    req.user.getProducts()
         .then(products => {
             res.render('admin/products',{
                 prods: products,
