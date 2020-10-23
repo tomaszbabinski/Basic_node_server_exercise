4aea3ddaefb2e59e071beffcb54b4998e4e3bd18
tomaszbabinski <tommybab@gmail.com>
2020-10-23 21:06:26 +0200

    Creating Cart and CartItem models so as relationships in app.js

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.set('views','views');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const User = require('./models/user');
-const Product = require('./models/product')
+const Product = require('./models/product');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item')
 
 const adminRoutes = require('./router/admin');
 const shopRoutes = require('./router/shop');
@@ -35,10 +37,14 @@ app.use(errorController.get404);
 
  Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
  User.hasMany(Product);
+ User.hasOne(Cart);
+ Cart.belongsTo(User);
+ Cart.belongsToMany(Product, { through: CartItem});
+ Product.belongsToMany(Cart, { through: CartItem});
 
 sequelize
-    .sync()
-    // .sync({force: true})
+    // .sync()
+    .sync({force: true})
     .then(result => {
         return User.findByPk(1)
     })
