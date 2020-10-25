b7771ba84ac1456d9c66cfe093642abd0effaced
tomaszbabinski <tommybab@gmail.com>
2020-10-25 14:33:19 +0100

    Updating postCart so it also adds product which is in the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -62,6 +62,7 @@ exports.getCart = (req,res,next) => {
 exports.postCart = (req,res,next) => {
     const prodId = req.body.productId;
     let fetchedCart;
+    let newQuantity = 1;
     req.user
         .getCart()
         .then( cart => {
@@ -72,25 +73,23 @@ exports.postCart = (req,res,next) => {
              let product;
              if(products.length > 0){
                  product = products[0];
-             }
-             let newQuantity = 1;
+             }     
              if(product){
-                 ///...
+                 const oldQuantity = product.cartItem.quantity;
+                 newQuantity = oldQuantity + 1;
+                 return product;                
              }
-             return Product.findByPk(prodId)
-                .then(product => {
-                    return fetchedCart.addProduct(product, {
-                        through: { quantity: newQuantity }
-                    });
-                })
-                .catch(err => console.log(err));
+             return Product.findByPk(prodId)                
         })
-        .then( () => {
+        .then(product => {
+            return fetchedCart.addProduct(product, {
+                through: { quantity: newQuantity}
+            })
+        })
+        .then(() => {
             res.redirect('/cart');
         })
         .catch(err => console.log(err))
- 
-
     // Product.findById(prodId, product => {
     //     Cart.addProduct(prodId, product.price);
     // })
