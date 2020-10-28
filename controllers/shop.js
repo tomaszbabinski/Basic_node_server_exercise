15294743557181da809b9478a9f4e0ceaf360ca7
tomaszbabinski <tommybab@gmail.com>
2020-10-28 18:56:21 +0100

    Creating postOrder method for moving items from cart to an order

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
-const e = require('express');
 
 exports.getProducts = (req, res, next) => {
     Product.findAll().then(products => {
@@ -111,6 +109,30 @@ exports.postCartDeleteProduct = (req,res,next) => {
         })
         .catch(err => console.log(err));
 }
+
+exports.postOrder = (req,res,next) => {
+  req.user.getCart()
+    .then(cart => {
+        return cart.getProducts()
+    })
+    .then(products => {
+        return req.user
+            .createOrder()
+            .then(order => {
+                return order.addProducts(
+                    products.map(product => {
+                        product.orderItem = { quantity: product.cartItem.quantity}
+                        return product;
+                    })
+                );
+            })
+            .catch(err=> console.log(err));
+    })
+    .then(result => {
+        res.redirect('/orders');
+    })
+    .catch(err => console.log(err));  
+};
 
 exports.getOrders = (req,res,next) => {
     res.render('shop/orders',{
