13f4e47febdbf94a8414b7485362a025f02e24fe
tomaszbabinski <tommybab@gmail.com>
2020-10-28 19:03:46 +0100

    Changing postOrder so it clears the Cart after making an order

--- controllers/shop.js
+++ controllers/shop.js
@@ -111,8 +111,10 @@ exports.postCartDeleteProduct = (req,res,next) => {
 }
 
 exports.postOrder = (req,res,next) => {
+  let fetchedCart;  
   req.user.getCart()
     .then(cart => {
+        fetchedCart = cart;
         return cart.getProducts()
     })
     .then(products => {
@@ -127,6 +129,9 @@ exports.postOrder = (req,res,next) => {
                 );
             })
             .catch(err=> console.log(err));
+    })
+    .then(result => {
+        return fetchedCart.setProducts(null);
     })
     .then(result => {
         res.redirect('/orders');
