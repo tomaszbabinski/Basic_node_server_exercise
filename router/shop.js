eac233220c79196f89cabc7c2f1a7b33d864c5e4
tomaszbabinski <tommybab@gmail.com>
2020-10-29 21:04:14 +0100

    Removing getCheckout from the router so the app won't breaks

--- router/shop.js
+++ router/shop.js
@@ -21,6 +21,5 @@ router.post('/create-order',shopController.postOrder);
 
 router.get('/orders',shopController.getOrders);
 
-router.get('/checkout',shopController.getCheckout);
 
 module.exports = router;
