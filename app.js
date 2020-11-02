215d7b94b55e4d470714100ddfb9cb8a525db3a3
tomaszbabinski <tommybab@gmail.com>
2020-11-02 20:52:24 +0100

    Routes modification/comment out so we can test functionalities step by step

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+
+const mongoConnect = require('./util/database').mongoConnect;
+const errorController = require('./controllers/error');
+
 const app = express();
-const mongoConnect = require('./util/database');
 
 app.set('view engine','ejs');
 app.set('views','views');
-
-const errorController = require('./controllers/error');
 
 const adminRoutes = require('./router/admin');
 // const shopRoutes = require('./router/shop');
@@ -17,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use((req,res,next) => {
- 
+    next();
 });
 
 app.use('/admin',adminRoutes);
@@ -26,4 +28,4 @@ app.use(errorController.get404);
 
 mongoConnect(() => {
     app.listen(3000);
-})
+});
--- router/admin.js
+++ router/admin.js
@@ -10,16 +10,16 @@ const adminController = require('../controllers/admin');
 //this normally gets /admin/add-product -> GET
 router.get('/add-product', adminController.getAddProduct);
 
-router.get('/products', adminController.getProducts);
+// router.get('/products', adminController.getProducts);
 
 //this normally gets /admin/add-product -> POST
 router.post('/add-product', adminController.postAddProduct)
 
-router.get('/edit-product/:productId',adminController.getEditProduct);
+// router.get('/edit-product/:productId',adminController.getEditProduct);
 
-router.post('/edit-product',adminController.postEditProduct);
+// router.post('/edit-product',adminController.postEditProduct);
 
-router.post('/delete-product',adminController.postDelete);
+// router.post('/delete-product',adminController.postDelete);
 
 module.exports = router;
 
