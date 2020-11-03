ccd38fcacf3836a127bdb8d16afc8d477c90dea6
tomaszbabinski <tommybab@gmail.com>
2020-11-03 20:40:24 +0100

    FetchAll method added

--- models/product.js
+++ models/product.js
@@ -19,6 +19,18 @@ class Product {
         console.log(err);
       });
   }
+
+  static fetchAll(){
+    const db = getDb();
+    return db.collection('products')
+            .find().toArray()
+            .then(products => {
+              console.log(products);
+              return products;
+            })
+            .catch( err => console.log(err));
+  }
+
 }
 
 module.exports = Product;
