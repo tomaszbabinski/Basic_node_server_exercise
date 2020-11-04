f338c19df14002628f8c5d1ef38d7d266fd872d9
tomaszbabinski <tommybab@gmail.com>
2020-11-04 19:15:10 +0100

    Adjustments to routes/controllers/views so findById work properly

--- controllers/shop.js
+++ controllers/shop.js
@@ -19,7 +19,7 @@ exports.getProduct = (req,res,next) => {
     //         path: '/products'
     //     });
     // }).catch(err=> console.log(err));
-    Product.findByPk(prodId)
+    Product.findById(prodId)
         .then((product) => {
             res.render('shop/product-detail', {
                 product: product,
--- router/shop.js
+++ router/shop.js
@@ -9,7 +9,7 @@ router.get('/', shopController.getIndex);
 
 router.get('/products',shopController.getProducts);
 
-// router.get('/products/:productId',shopController.getProduct);
+router.get('/products/:productId',shopController.getProduct);
 
 // router.get('/cart',shopController.getCart);
 
