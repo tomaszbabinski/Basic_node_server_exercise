6dfc5df5be1336a8af01d79c454d0f5621d38299
tomaszbabinski <tommybab@gmail.com>
2020-11-10 13:18:44 +0100

    Route and shop controller changes so postToCart works - only for new product

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use((req,res,next) => {
     User.findById('5fa9a2e146d014729553b11c')
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => console.log(err));
