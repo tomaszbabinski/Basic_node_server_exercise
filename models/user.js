9aef826edd36a52004529ff4f643c13ac5b8830b
tomaszbabinski <tommybab@gmail.com>
2020-11-10 13:17:21 +0100

    Chaning addToCart method so cart stores only quantity and id references

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ class User{
         //     return cp._id === product._id;
         // });
         
-        const updatedCart = { items: [{...product, quantity: 1}] };
+        const updatedCart = { items: [{productId : new ObjectId(product._id), quantity: 1}] };
         const db = getDb();
         return db
             .collection('users').updateOne(
