683c93d9ac1688d7063db7fdfbf73e1de511d6b7
tomaszbabinski <tommybab@gmail.com>
2020-11-10 12:46:38 +0100

    Adding a product to an empty cart - moving logic from cart model to cart property of User class

--- models/user.js
+++ models/user.js
@@ -4,14 +4,31 @@ const mongodb = require('mongodb');
 const ObjectId = mongodb.ObjectId;
 
 class User{
-    constructor(username,email){
+    constructor(username,email,cart,id){
         this.username = username;
         this.email = email;
+        this.cart = cart; // {items: []}
+        this._id = id;
     }
 
     save(){
         const db = getDb();
         return db.collection('users').instertOne(this);
+    }
+
+    addToCart(product){
+        // const cartProduct = this.cart.items.findIndex(cp => {
+        //     return cp._id === product._id;
+        // });
+        
+        const updatedCart = { items: [{...product, quantity: 1}] };
+        const db = getDb();
+        return db
+            .collection('users').updateOne(
+            {_id: new ObjectId(this._id)},
+            { $set: { cart: updatedCart}}
+            );
+
     }
 
     static findById(userId){
