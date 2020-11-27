825e1e3255fe766b6c6ec61e128d7b286b43a2ec
tomaszbabinski <tommybab@gmail.com>
2020-11-27 17:39:15 +0100

    Adding an order functionality - saving in db and clearing cart

--- models/user.js
+++ models/user.js
@@ -70,6 +70,20 @@ class User{
             );
     }
 
+    addOrder(){
+        const db = getDb();
+        return db.collection('orders')
+            .insertOne(this.cart)
+            .then(result => {
+                this.cart = {items: [] };
+                return db
+                .collection('users').updateOne(
+                 {_id: new ObjectId(this._id)},
+                 { $set: { cart: {items: [] } } }
+                );
+        });
+    }
+
     static findById(userId){
         const db = getDb();
         return db.collection('users').find({_id: new ObjectId(userId)}).next()
