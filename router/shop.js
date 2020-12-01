700114da246ce315c6465b011faf8862ba393404
tomaszbabinski <tommybab@gmail.com>
2020-12-01 20:52:03 +0100

    Fetching products using mongoose

--- router/shop.js
+++ router/shop.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 const shopController = require('../controllers/shop');
 
-// router.get('/', shopController.getIndex);
+router.get('/', shopController.getIndex);
 
-// router.get('/products',shopController.getProducts);
+router.get('/products',shopController.getProducts);
 
 // router.get('/products/:productId',shopController.getProduct);
 
