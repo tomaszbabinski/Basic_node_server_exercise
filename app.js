47382e905a6f86015d815787b97195e02906b044
tomaszbabinski <tommybab@gmail.com>
2020-12-04 19:32:36 +0100

    Creating and using user once the app starts

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -20,14 +20,14 @@ const shopRoutes = require('./router/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
 
-// app.use((req,res,next) => {
-//     User.findById('5fa9a2e146d014729553b11c')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch(err => console.log(err));
-// });
+app.use((req,res,next) => {
+    User.findById('5fca7e0aee225c5d702e9fcc')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
@@ -36,6 +36,18 @@ app.use(errorController.get404);
 mongoose
     .connect('mongodb+srv://tommybab_node:i3USmkwJ0b8clDGJ@cluster0.edvp1.mongodb.net/shop?retryWrites=true&w=majority')
     .then(result => {
+        User.findOne().then(user => {
+            if(!user){
+                const user = new User({
+                    name: 'Tom',
+                    email: 'tom@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        })
         app.listen(3000);
     })
     .catch(err => {
