50808cdc6039542cd29405d2637df371699889c6
tomaszbabinski <tommybab@gmail.com>
2020-12-09 20:16:36 +0100

    Setting up a cookie which stores information is user is logged in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
 exports.getLogin = (req,res,next) => {
-            res.render('auth/login',{
-                path: '/login',
-                pageTitle: 'Login',
-                isAuthenticated: req.isLoggedIn
-            });
+    const isLoggedIn = req.get('Cookie').split('=')[1];
+        res.render('auth/login',{
+            path: '/login',
+            pageTitle: 'Login',
+            isAuthenticated: isLoggedIn
+        });
 };
 
 exports.postLogin = (req,res,next) => {
-   req.isLoggedIn = true; 
+   res.setHeader('Set-Cookie','loggedIn=true');
    res.redirect('/');
 };
