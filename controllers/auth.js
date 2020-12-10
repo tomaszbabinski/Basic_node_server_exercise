c1f1067f2bd0f530299833168fd0d2dba6bac312
tomaszbabinski <tommybab@gmail.com>
2020-12-10 21:39:44 +0100

    Storing the isLoggedIn information in the session

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,6 @@ exports.getLogin = (req,res,next) => {
 };
 
 exports.postLogin = (req,res,next) => {
-   res.setHeader('Set-Cookie','loggedIn=true');
+   req.session.isLoggedIn = true;
    res.redirect('/');
 };
