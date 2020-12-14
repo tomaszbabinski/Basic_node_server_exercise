14c3ac2adf12659e01e6f30728ef40726dd04628
tomaszbabinski <tommybab@gmail.com>
2020-12-14 19:54:59 +0100

    Added simple authentication flow

--- app.js
+++ app.js
@@ -55,20 +55,8 @@ app.use(errorController.get404);
 mongoose
     .connect(MONGODB_URI)
     .then(result => {
-        User.findOne().then(user => {
-            if(!user){
-                const user = new User({
-                    name: 'Tom',
-                    email: 'tom@test.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        })
         app.listen(3000);
     })
     .catch(err => {
         console.log(error);
-    })
+    });
