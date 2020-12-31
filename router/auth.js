b8836f34c91a7ae1252d09ea7de4e80f3b66b1f4
tomaszbabinski <tommybab@gmail.com>
2020-12-31 10:51:34 +0100

    Added custom validators for test purposes

--- router/auth.js
+++ router/auth.js
@@ -11,7 +11,16 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email.'), authController.postSignup);
+router.post('/signup', 
+    check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email.')
+    .custom((value, {req}) => {
+        if(value === 'test@test.com'){
+            throw new Error('This email is forbidden');
+        }
+        return true;
+    }), authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
