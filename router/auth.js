7d79cbe4f16ad3f2e00d6d9c59d52fae080eddde
tomaszbabinski <tommybab@gmail.com>
2020-12-31 11:00:29 +0100

    Added password validator using the body method

--- router/auth.js
+++ router/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -11,16 +11,24 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', 
-    check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email.')
-    .custom((value, {req}) => {
-        if(value === 'test@test.com'){
-            throw new Error('This email is forbidden');
-        }
-        return true;
-    }), authController.postSignup);
+router.post('/signup',
+    [ 
+        check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, {req}) => {
+            if(value === 'test@test.com'){
+                throw new Error('This email is forbidden');
+            }
+            return true;
+        }),
+        body('password',
+            'Please enter a password with only numbers and text and at least 5 characters long'
+        )
+        .isLength({ min: 5})
+        .isAlphanumeric()
+    ], 
+    authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
