8106c80146c00c218ce2e639e9db5c84d65b6eb8
tomaszbabinski <tommybab@gmail.com>
2021-01-04 21:07:36 +0100

    Preparing admin routes for adding and editing products - validation

--- router/admin.js
+++ router/admin.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path');
 
 const rootDir = require('../util/path');
+const { body } = require('express-validator');
 
 const products = [];
 const adminController = require('../controllers/admin');
@@ -12,13 +13,23 @@ const router = express.Router()
 router.get('/add-product', isAuth, adminController.getAddProduct);
 
 //this normally gets /admin/add-product -> POST
-router.post('/add-product', isAuth, adminController.postAddProduct)
+router.post('/add-product', [
+   body('title').isString().isLength({min: 3}).trim(),
+   body('imageUrl').isURL(),
+   body('price').isFloat(),
+   body('description').isLength({min: 5, max: 400}).trim()
+], isAuth, adminController.postAddProduct)
 
 router.get('/products', isAuth, adminController.getProducts);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product',[
+    body('title').isString().isLength({min: 3}).trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description').isLength({min: 5, max: 400}).trim()
+ ], isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDelete);
 
