5ad959b625b331601bff600d40a7c848e83781be
tomaszbabinski <tommybab@gmail.com>
2021-01-07 20:57:47 +0100

    Using expres.js error handling middleware

--- app.js
+++ app.js
@@ -71,6 +71,11 @@ app.use(authRoutes);
 app.get('/500',errorController.get500);
 app.use(errorController.get404);
 
+app.use((error,req,res,next) => {
+    // res.status(error.httpStatusCode).render('500');
+    res.redirect('/500');
+});
+
 mongoose
     .connect(MONGODB_URI)
     .then(result => {
--- controllers/admin.js
+++ controllers/admin.js
@@ -61,7 +61,9 @@ exports.postAddProduct = (req, res, next) => {
             //     errorMessage: 'Database validation failed, please try again',
             //     validationErrors: []
             // });
-            res.redirect('/500');
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
         })
 }
 
@@ -88,7 +90,11 @@ exports.getEditProduct = (req, res, next) => {
                 validationErrors: []
             });
         })
-        .catch(err => console.log(err))
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        })
 }
 
 
