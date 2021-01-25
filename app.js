8868e488654f2010d802240ff0f5d4f1435b660e
tomaszbabinski <tommybab@gmail.com>
2021-01-25 19:37:41 +0100

    Modifying multer to add filename and path for saving

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ const store = new MongoDBStore({
 
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
 app.set('view engine','ejs');
 app.set('views','views');
 
@@ -31,7 +40,7 @@ const authRoutes = require('./router/auth');
 const { collection } = require('./models/user');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(multer().single('image'));
+app.use(multer({storage: fileStorage}).single('image'));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(session({
     secret: 'my secret',
