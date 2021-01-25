c37ee74e751c6580ac713a043579d71d5022f367
tomaszbabinski <tommybab@gmail.com>
2021-01-25 20:15:48 +0100

    Storing file data in the db

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,10 +14,28 @@ exports.getAddProduct = (req, res, next) => {
 
 
 exports.postAddProduct = (req, res, next) => {
-    const imageUrl = req.body.image;
+    const image = req.file;
     const price = req.body.price;
     const description = req.body.description;
     const title = req.body.title;
+
+    if(!image){
+        return res.status(422).render('admin/edit-product',{
+            pageTitle: 'Add product', 
+            path: '/admin/add-product',
+            editing: false,
+            hasError: true,
+            product: {
+                title: title,
+                image: image,
+                price: price,
+                description: description
+            },
+            errorMessage: 'This is not valid image type',
+            validationErrors: []
+        });
+    }
+
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(422).render('admin/edit-product',{
@@ -27,7 +45,7 @@ exports.postAddProduct = (req, res, next) => {
             hasError: true,
             product: {
                 title: title,
-                imageUrl: imageUrl,
+                image: image,
                 price: price,
                 description: description
             },
@@ -35,6 +53,9 @@ exports.postAddProduct = (req, res, next) => {
             validationErrors: errors.array()
         });
     }
+
+    const imageUrl = image.path;
+
     const product = new Product({
         title: title,
         price: price,
@@ -102,7 +123,7 @@ exports.postEditProduct = (req,res,next) => {
     const prodId = req.body.productId;
     const updatedTitle = req.body.title;
     const updatedPrice = req.body.price;
-    const updatedImageUrl = req.body.imageUrl;
+    const updatedImageUrl = req.file
     const updatedDescription = req.body.description;
     const errors = validationResult(req);
     if(!errors.isEmpty()){
@@ -113,7 +134,6 @@ exports.postEditProduct = (req,res,next) => {
             hasError: true,
             product: {
                 title: updatedTitle,
-                imageUrl: updatedImageUrl,
                 price: updatedPrice,
                 description: updatedDescription,
                 _id: prodId
@@ -130,7 +150,10 @@ exports.postEditProduct = (req,res,next) => {
         product.title = updatedTitle;
         product.price = updatedPrice;
         product.description = updatedDescription;
-        product.imageUrl = updatedImageUrl;
+        if(image){
+            product.imageUrl = image.path;
+        }
+        
         return product.save()
             .then(result => {
             console.log('Updated product!!!')
