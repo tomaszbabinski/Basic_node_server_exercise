73aa5179cecfe6018043fee7fd82e1e5b073da85
tomaszbabinski <tommybab@gmail.com>
2021-02-02 16:31:58 +0100

    Added headers related to the filetype

--- controllers/shop.js
+++ controllers/shop.js
@@ -161,6 +161,8 @@ exports.getInvoice = (req,res, next) => {
         if(err){
             return next(err);
         }
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition','inline; filename="'+invoiceName + '"');
         res.send(data);
     });
 }
