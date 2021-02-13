c68c6ce0dd71c1357d35a1b05cf5744c60799725
tomaszbabinski <tommybab@gmail.com>
2021-02-13 20:10:00 +0100

    Preparing shop controller for loading products with pagination functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -39,15 +39,27 @@ exports.getProduct = (req,res,next) => {
 
 exports.getIndex = (req,res,next) => {
   const page = req.query.page;
+  let totalItems;
 
-    Product.find()
-      .skip((page-1)*ITEMS_PER_PAGE)
-      .limit(ITEMS_PER_PAGE)
-      .then(products => {
+  Product
+    .find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+                    .skip((page-1)*ITEMS_PER_PAGE)
+                    .limit(ITEMS_PER_PAGE)
+    }).then(products => {
         res.render('shop/index',{
             prods: products,
             pageTitle: 'Shop',
-            path: '/'
+            path: '/',
+            totalProducts: totalItems,
+            hasNextPage: ITEMS_PER_PAGE *page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page -1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)  
          });
     }).catch(err => {
         const error = new Error(err);
