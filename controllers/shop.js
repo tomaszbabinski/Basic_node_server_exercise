212650bb114eea6587fba10e9ca9db561ea528de
tomaszbabinski <tommybab@gmail.com>
2021-02-15 22:44:38 +0100

    Reusing the pagination code in other place (product-list) & removing absolute path in the pagination.ejs

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,13 +7,30 @@ const ITEMS_PER_PAGE = 1;
 const PDFDocument = require('pdfkit');
 
 exports.getProducts = (req, res, next) => {
-    Product.find().then(products => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product
+    .find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+                    .skip((page-1)*ITEMS_PER_PAGE)
+                    .limit(ITEMS_PER_PAGE)
+    }).then(products => {
         res.render('shop/product-list',{
             prods: products,
-            pageTitle: 'All Products',
-            path: '/products'
+            pageTitle: 'Products',
+            path: '/products',
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)  
          });
-    })  .catch(err => {
+    }).catch(err => {
         const error = new Error(err);
         error.httpStatusCode = 500;
         return next(error);
