7e743b1422c6faa3b6c7f16943e7c1b1915c4005
tomaszbabinski <tommybab@gmail.com>
2021-02-15 22:29:16 +0100

    Adjusting getIndex method so page constant is a number and won't destroy the view

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 const fs = require('fs');
 const path = require('path');
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 const PDFDocument = require('pdfkit');
 
@@ -38,7 +38,7 @@ exports.getProduct = (req,res,next) => {
 }
 
 exports.getIndex = (req,res,next) => {
-  const page = req.query.page;
+  const page = +req.query.page || 1;
   let totalItems;
 
   Product
@@ -54,11 +54,11 @@ exports.getIndex = (req,res,next) => {
             prods: products,
             pageTitle: 'Shop',
             path: '/',
-            totalProducts: totalItems,
-            hasNextPage: ITEMS_PER_PAGE *page < totalItems,
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
             hasPreviousPage: page > 1,
             nextPage: page + 1,
-            previousPage: page -1,
+            previousPage: page - 1,
             lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)  
          });
     }).catch(err => {
