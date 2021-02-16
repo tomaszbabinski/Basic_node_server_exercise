a4bc088e5f87e02972f1c754a43bc3202c359a74
tomaszbabinski <tommybab@gmail.com>
2021-02-16 21:48:28 +0100

    Added client side request for deleting a product

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,18 @@
 const deleteProduct = (btn) => {
-    const prodId = btn.parentNode.querySelector('name=productId').value;
-    const csrf = btn.parentNode.querySelector('name=_csrf').value;
+    const prodId = btn.parentNode.querySelector('[name=productId]').value;
+    const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+    fetch('/admin/product/'+prodId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    })
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => {
+            console.log(err)
+        });
+
 };
