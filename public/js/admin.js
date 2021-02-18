9d867c9cdaa48320b6ec84681f5d968fdb6436c8
tomaszbabinski <tommybab@gmail.com>
2021-02-18 20:49:28 +0100

    Simulating deleting a product with no reloading of admin products page

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,7 @@
 const deleteProduct = (btn) => {
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+    const productElement = btn.closest('article');
 
     fetch('/admin/product/'+prodId, {
         method: 'DELETE',
@@ -9,7 +10,11 @@ const deleteProduct = (btn) => {
         }
     })
         .then(result => {
-            console.log(result);
+            return result.json()
+        })
+        .then(data => {
+            console.log(data);
+            productElement.parentNode.removeChild(productElement);
         })
         .catch(err => {
             console.log(err)
